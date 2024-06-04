1ee677228a30b19714d35e8d0e6af902da7072f8
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-06-04 20:36:43 -0300

    First Commit

---
+++ Exercicio02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Exemplo02
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Vamos descobrir se o número é Positivo Negativo ou Zero!");
+        Console.WriteLine("Digite um número:");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        if (numero == 0)
+        {
+            Console.WriteLine("O número " + numero + " é Zero!");
+        }
+        else if (numero > 0)
+        {
+            Console.WriteLine("O número " + numero + " é positivo!");
+        }
+        else
+        {
+            Console.WriteLine("O número " + numero + " é negativo!");
+        }
+    }
+}
---
+++ Exercicio05.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Net.Http.Headers;
+
+class Exemplo05
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Vamos verificar se um aluno foi aprovado ou não!");
+        Console.WriteLine("Digite um valor para a primeira nota:");
+        float nota1 = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Digite um valor para a segunda nota:");
+        float nota2 = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Digite um valor para a terceira nota:");
+        float nota3 = Convert.ToSingle(Console.ReadLine());
+
+        float media = (nota1 + nota2 + nota3) / 3;
+
+        if (media < 7)
+        {
+            Console.WriteLine("O aluno foi aprovado com média " + $"{media:0.00}");
+        }
+        else if (media >= 5 && media < 7)
+        {
+            Console.WriteLine("O aluno está de recuperação com média " + $"{media:0.00}");
+        }
+        else
+        {
+            Console.WriteLine("O aluno foi Reprovado com média " + $"{media:0.00}");
+        }
+    }
+}
