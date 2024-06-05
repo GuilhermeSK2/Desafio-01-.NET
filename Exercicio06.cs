371a591c76229893294b00e493d33cf9777e4117
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-06-04 21:16:21 -0300

    First Commit

---
+++ Exercicio06.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Exemplo06
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Vamos descobrir se um ano é bissexto ou não!");
+        int ano = Convert.ToInt32(Console.ReadLine());
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))
+        {
+            Console.WriteLine("O ano é bissexto!");
+        }
+        else
+        {
+            Console.WriteLine("O ano não é bissexto!");
+        }
+    }
+}
