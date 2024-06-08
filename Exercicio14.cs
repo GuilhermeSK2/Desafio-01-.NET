b230d8425a1d4977133de2079d621c87330ac554
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-06-08 12:06:52 -0300

    Add files via upload

---
+++ Exercicio14.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Exemplo14
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Vamos descobrir se um número é negativo ou positivo!");
+        Console.WriteLine("Digite um número:");
+        double numero = Convert.ToDouble(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("A Raiz quadrada do número " + numero + " é " + (Math.Sqrt(numero)));
+        }
+        else
+        {
+            Console.WriteLine("O número " + numero + " ao quadrado é " + (numero + numero));
+        }
+    }
+}
