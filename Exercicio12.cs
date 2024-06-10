bb2bc8c74c0377f0dce95ecf851103de9e56f983
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-06-10 00:59:56 -0300

    Update Exercicio12.cs

--- Exercicio12.cs
+++ Exercicio12.cs
@@ -5,7 +5,7 @@ class Exemplo12
     public static void Main(string[] args)
     {
         Console.WriteLine("Vamos descobrir se um número é maior que 100, se não for exibiremos o dobro dele!");
-        Console.WriteLine("Digite um número!");
+        Console.WriteLine("Digite um número:");
         int numero = Convert.ToInt32(Console.ReadLine());
         if (numero > 100 )
         {
